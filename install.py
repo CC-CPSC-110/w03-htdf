ddb8e009c211192534bdd217fdd0373f307ec3e1
phb <pbucci@gmail.com>
2024-09-17 14:53:04 -0700

    Update install.py

--- install.py
+++ install.py
@@ -6,6 +6,7 @@ def install(package: str, *args) -> None:
 
 install("git+https://github.com/CC-CPSC-110/cs110.git", "--force")
 install("pygame==2.6.0")
+install("requests")
 
 # subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "pip"])
 
